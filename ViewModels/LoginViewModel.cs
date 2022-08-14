81968e0f94ed07439301c5a01ebe85716a5bdb12
Haydn202 <haydnthomson@hotmail.com>
2022-08-14 22:14:27 +1200

    dont display password

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace blog.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blog.ViewModels;
 
 public class LoginViewModel
 {
     public string UserName { get; set; }
+    [DataType(DataType.Password)]
     public string Password { get; set; }
 }
